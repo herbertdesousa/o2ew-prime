f60dafdb39c9db17c666d6c5ec47d46285a7b6c8
herbertdesousa <sousaherbert138@gmail.com>
2024-11-01 08:44:43 -0300

    feat: goal context

---
+++ contexts/goal-context.ts
@@ -0,0 +1,47 @@
+import { ViewPagerRef } from "@/components/ViewPager/react-augment";
+import { DI } from "@/controllers/DI";
+import { Goal } from "@/entities/goal";
+import { create } from "zustand";
+
+type AsyncState<Data> =
+  | { state: 'LOADING' }
+  | { state: 'ERROR' }
+  | { state: 'SUCCESS'; data: Data };
+
+type Context = {
+  // VP = view pager
+  goalVPRef: React.RefObject<ViewPagerRef<Goal>>;
+  goals: AsyncState<Goal[]>;
+  selectedGoal: {
+    state: Goal | null;
+    index: number;
+  };
+  onChangeSelection(goal: Goal): void;
+}
+
+const goalVPRef = { current: null } as React.RefObject<ViewPagerRef<Goal>>;
+
+export function asyncGoalsToState(asyncGoals: AsyncState<Goal[]>) {
+  return asyncGoals.state === 'SUCCESS' ? asyncGoals.data : [];
+}
+
+export const useGoal = create<Context>((set) => {
+  DI.goal.GetGoals()
+    .then(data => set({ goals: { state: 'SUCCESS', data } }))
+    .catch(() => set({ goals: { state: 'ERROR' } }))
+
+  return {
+    goalVPRef,
+    goals: { state: 'LOADING' },
+    selectedGoal: { state: null, index: 0 },
+    onChangeSelection: (_goal) => {
+      set(({ goals }) => ({
+        selectedGoal: {
+          state: _goal,
+          index: asyncGoalsToState(goals)
+            .findIndex(g => g.$clientId === _goal.$clientId)
+        }
+      }));
+    }
+  };
+})
